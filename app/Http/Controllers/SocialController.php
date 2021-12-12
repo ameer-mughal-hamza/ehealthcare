413a6d81c98745b2c3df10aa9db4c0a0d5523b19
ameer-mughal-hamza <hamza6417307@gmail.com>
2021-12-12 18:59:00 +0100

    Google login and register for patients

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Models\Patient;
+use Exception;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
@@ -11,76 +13,60 @@ use Illuminate\Support\Facades\Hash;
 
 class SocialController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function redirectToGoogle()
     {
         return Socialite::driver('google')->redirect();
     }
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function handleGoogleCallback()
     {
         try {
 
             $user = Socialite::driver('google')->user();
-
-            dd($user);
-
-            $finduser = User::where('google_id', $user->id)->first();
-
-            if($finduser){
-
+            $finduser = User::where('email', $user->email)->first();
+            if ($finduser) {
                 Auth::login($finduser);
-
                 return redirect('/home');
+            } else {
+                $patient = new Patient();
+                $patient->mrn = getRandomMRN();
 
-            }else{
-                $newUser = User::create([
-                    'name' => $user->name,
-                    'email' => $user->email,
-                    'google_id'=> $user->id,
-                    'password' => encrypt('123456dummy')
-                ]);
+                $newUser = new User();
+                if ($user->user->given_name && $user->user->family_name) {
+                    $newUser->first_name = $user->user->given_name;
+                    $newUser->last_name = $user->user->family_name;
+                } else {
+                    $name = preg_split('#\s+#', $user->name, 2);
+
+                    $newUser->first_name = $name[0];
+                    $newUser->last_name = $name[1];
+                }
+
+                $newUser->email = $user->email;
+                $newUser->role = 3;
+                $newUser->password = Hash::make(encrypt(rand(10000000, 99999999)));
+                $newUser->save();
+                $newUser->patient()->save($patient);
 
                 Auth::login($newUser);
-
                 return redirect('/home');
             }
-
         } catch (Exception $e) {
-            dd($e->getMessage());
+            return redirect('/home');
         }
     }
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function redirectToFacebook()
     {
         return Socialite::driver('facebook')->redirect();
     }
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function handleFacebookCallback()
     {
         try {
             $user = Socialite::driver('facebook')->user();
             $finduser = User::where('email', $user->email)->first();
-            if($finduser){
+            if ($finduser) {
                 Auth::login($finduser);
                 return redirect('/home');
             } else {
@@ -103,7 +89,7 @@ class SocialController extends Controller
             }
 
         } catch (Exception $e) {
-            dd($e->getMessage());
+            return redirect('/home');
         }
     }
 }
